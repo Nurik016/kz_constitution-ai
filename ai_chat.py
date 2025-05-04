b305729b9bd3db0b52827e81e70dc976c6cd253a
Nurdaulet <149077023+Nurik016@users.noreply.github.com>
2025-05-04 18:39:58 +0500

    Update ai_chat.py

--- ai_chat.py
+++ ai_chat.py
@@ -1,29 +1,12 @@
-# import os
 import textwrap
 import google.generativeai as genai
 
-# from dotenv import load_dotenv
-
-
-# load_dotenv()
 
 def to_console_markdown(text):
   text = text.replace('•', '  *')  # маркеры
   text = textwrap.indent(text, '> ')  # отступ
   return text
 
-# GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-# genai.configure(api_key=GOOGLE_API_KEY)
-
-# To see llm versions
-# for m in genai.list_models():
-#   if 'generateContent' in m.supported_generation_methods:
-#     print(m.name)
-
-# model = genai.GenerativeModel(model_name='gemini-2.0-flash')
-# response = model.generate_content("Hello, can u tell me best phonk?")
-# answer = to_console_markdown(response.text)
-# print(answer)
 
 def ai_chat(text: None, api_key: None) -> None:
   genai.configure(api_key=api_key)
@@ -32,4 +15,3 @@ def ai_chat(text: None, api_key: None) -> None:
   response = model.generate_content(text)
   answer = to_console_markdown(response.text)
   return answer
-
