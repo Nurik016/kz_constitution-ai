d7eeeb6921d4f41270379f34a21c8ca12554de6d
Nurdaulet <149077023+Nurik016@users.noreply.github.com>
2025-05-04 18:40:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import streamlit as st
 
 from ai_chat import ai_chat
+from handler_files import extract_text_from_file
+from vector_store import save_to_vectorstore, query_vectorstore, save_query_answer
 
-st.title('ZanBot')
 
-
+st.title('⚖️ ZanBot')
 
 with st.sidebar:
     gemini_api_key = st.text_input("Gemini API Key", type="password")
+    uploaded_files = st.file_uploader("Upload files", accept_multiple_files=True, type=["txt", "pdf", "docx"])
 
-    uploaded_files = st.file_uploader("Upload a file", accept_multiple_files=True)
+    all_text = []
 
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-        st.write(bytes_data)
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            extracted = extract_text_from_file(uploaded_file)
+            st.success(f"📄 {uploaded_file.name} loaded")
+            all_text.append(extracted)
 
-
+        save_to_vectorstore(all_text)
 
 if 'messages' not in st.session_state:
     st.session_state['messages'] = []
@@ -26,13 +29,17 @@ for message in st.session_state.messages:
         st.markdown(message['content'])
 
 if promt := st.chat_input('What can I do to help?'):
+    context = query_vectorstore(promt)
+    full_prompt = f"Context:\n{context}\n\nUser question:\n{promt}"
+
     st.chat_message('user').markdown(promt)
-    st.session_state.messages.append({'role':'user', 'content':promt})
+    st.session_state.messages.append({'role': 'user', 'content': promt})
 
-    answer = ai_chat(promt, gemini_api_key) #ai
+    response = ai_chat(full_prompt, gemini_api_key)
 
-    response = answer
+    save_query_answer(promt, response)
+
     with st.chat_message('assistant'):
         st.markdown(response)
 
-    st.session_state.messages.append({'role':'assistant', 'content':response})
+    st.session_state.messages.append({'role': 'assistant', 'content': response})
